ef4d3cabbe71f74612fc0778cd19cd3382d3593b
dgeorgik <gosha_pustovalov@mail.ru>
2023-09-21 23:02:22 +0300

    added README.md

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author georgijpustovalov
+ * @project TestArgoCDProject
+ * @Date 21.09.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
